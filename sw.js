525801cf61e9bdf8171e4169411a77449bb5b5e9
Rahul Choudhury <rchoudhury@gmail.com>
2016-11-05 16:55:28 -0700

    Update site to incorporate accessibility improvements and add AMP and a service worker

---
+++ sw.js
@@ -0,0 +1,18 @@
+
+importScripts('/node_modules/sw-toolbox/sw-toolbox.js');
+
+const config = {};
+
+config.filesToCache = [
+  './',
+  '/fonts/IntroBlackCapsRegular.otf',
+  '/fonts/IntroBookCapsRegular.otf',
+];
+
+toolbox.options.debug = true;
+toolbox.router.default = toolbox.networkOnly;
+toolbox.router.get('/(.*)', toolbox.fastest);
+toolbox.router.get('/(.*)', toolbox.cacheFirst, {origin: 'https://storage.googleapis.com'});
+toolbox.router.get('/(.*)', toolbox.cacheFirst, {origin: 'https://cdn.ampproject.org'});
+
+toolbox.precache(config.filesToCache);
